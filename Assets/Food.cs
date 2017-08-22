efff0beade350fc9dbc460c3c2cb25766be34e75
Guillaume Noguera <Guillaume Noguera>
2017-08-22 16:20:34 +0200

    initial commit

---
+++ Assets/Food.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+    private bool cooked = false;
+    private bool burnt = false;
+    private bool isBeingCooked = false;
+    public int timeToCook;
+    int counter = 0;
+    int timeSpentCooking = 0;
+
+    public void setTimeToCook(int _time) {
+        this.timeToCook = _time;
+    }
+
+    public void setCooking(bool _b) {
+        this.isBeingCooked = _b;
+    }
+	// Update is called once per frame
+	void Update () {
+        if (isBeingCooked) {
+            counter++;
+            if (counter % 60 == 0) {
+                timeSpentCooking++;
+                counter = 0;
+            }
+        }
+        if (timeSpentCooking == timeToCook) {
+            Debug.Log("cooked");
+            cooked = true;
+            this.GetComponent<Renderer>().material.color = Color.red;
+            //Renderer rend = this.GetComponent<Renderer>();
+            //rend.material.shader = Shader.Find("Specular");
+            //rend.material.SetColor("_SpecColor", Color.red);
+        }
+        if (timeSpentCooking == (int)2 * timeToCook) {
+            Debug.Log("overcooked lol");
+            burnt = true;
+            this.GetComponent<Renderer>().material.color = Color.black;
+            //Renderer rend = this.GetComponent<Renderer>();
+            //rend.material.shader = Shader.Find("Specular");
+            //rend.material.SetColor("_SpecColor", Color.black);
+        }
+    }
+}
---
+++ Assets/Scrips/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public Transform player;
+    private float lerpSpeed = 0.1f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 target = Vector3.Lerp(this.transform.forward, player.transform.position, lerpSpeed * Time.deltaTime);
+        transform.LookAt(player);
+	}
+}
