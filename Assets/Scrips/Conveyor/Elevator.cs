e2899a8029970af3c5beb5f5c353d1f50e3a7815
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-08-31 11:17:04 +0200

    Elevator nice and working (pusher ready to jam)

--- Assets/Scrips/Conveyor/Elevator.cs
+++ Assets/Scrips/Conveyor/Elevator.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class Elevator : MonoBehaviour {
 
-	public GameObject[] plates = new GameObject[5];
-	
-	// Update is called once per frame
+	public List<GameObject> plates;
+	public float speed = 0.02f;
+	public float endPos = 1.5f;
+	public float startPos = -1.5f;
+	public Vector3 startPoint;
+	public Vector3 endPoint;
+	private Vector3 conveyVec;
+	private Pusher pusher;
+
+	void Awake()
+	{
+		conveyVec = new Vector3 (0, speed, 0);
+		startPoint = new Vector3 (0, startPos, 0);
+		endPoint = new Vector3 (0, endPos, 0);
+	}
 	void Update () {
-		
+		foreach (GameObject plate in plates) 
+		{
+			plate.transform.Translate (conveyVec);
+			if (plate.transform.localPosition.y >= endPos) //teleport back to end pos
+			{
+				//pusher = plate.GetComponent<Pusher> ();
+				//pusher.push ();
+				plate.transform.position -= endPoint - startPoint;
+			}
+		}
 	}
 }
---
+++ Assets/Scrips/Conveyor/Pusher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher: MonoBehaviour
+{
+	private List<Rigidbody> plates = null;
+	private Vector3 pushVec;
+	private float strength = 1f;
+
+	void Awake()
+	{
+		pushVec = new Vector3 (strength, 0, 0);
+	}
+	public void push (){
+		foreach (Rigidbody plate in plates) 
+		{
+			plate.AddForce (pushVec);
+		}
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		Rigidbody body = col.gameObject.GetComponent<Rigidbody> ();
+		if(body!=null)
+			plates.Add (body);
+	}
+	void OnCollisionExit(Collision col)
+	{
+		Rigidbody body = col.gameObject.GetComponent<Rigidbody> ();
+		if (body != null)
+			plates.Remove (body);
+	}
+	public void setStrength(float powaaaa)
+	{
+		strength = powaaaa;
+	}
+	public void setPushVec(Vector3 vector)
+	{
+		pushVec = vector.normalized * strength;
+	}
+}
+
+
