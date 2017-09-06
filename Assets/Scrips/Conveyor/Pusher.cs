02784d7c80e998707eb05827e1939d168997d649
florian.fasmeyer <florian.fasmeyer@he-arc.ch>
2017-09-06 14:48:54 +0200

    Auto furnace, oldFile gamemanager moved in current files, dispenced prefab moved in prefab/conveyor/Dispencer, elevator prefab sound fixed.

--- Assets/Scrips/Conveyor/Pusher.cs
+++ Assets/Scrips/Conveyor/Pusher.cs
@@ -9,14 +9,21 @@ public class Pusher: MonoBehaviour
 	private float strength = 1f;
 	private Rigidbody thisRb;
 
+	public AudioClip throwSound = null;
+	private AudioSource source = null;
+
 	void Awake()
 	{
 		pushVec = new Vector3 (1, 1, 0).normalized;
 		onPlate = new List<Rigidbody> ();
 		thisRb = GetComponent<Rigidbody> ();
+
+		source = GetComponent<AudioSource> ();
 	}
 	public void push ()
 	{
+		if(onPlate.Count > 0 && source != null && throwSound != null)
+			source.PlayOneShot (throwSound);
 		foreach (Rigidbody objit in onPlate)
 			objit.velocity = pushVec*strength;
 	}
--- Assets/Scrips/Furniture/AutoFurnace.cs
+++ Assets/Scrips/Furniture/AutoFurnace.cs
@@ -5,25 +5,41 @@ using UnityEngine;
 class AutoFurnace : abstractFurniture 
 {
 	public ParticleSystem smoke = null;
-	public Light furnaceLight = null;
-	public Animator animator = null;
+
+	public AudioClip processSound = null;
+	public AudioClip takeSound = null;
+	public AudioClip putSound = null;
+	public AudioClip bellSound = null;
+	private AudioSource source = null;
+
+	private bool bellOnce = true;
 
 	void Start() 
 	{
-		animator = GetComponent<Animator>();
 		updateFurniture ();
+		source = GetComponent<AudioSource> ();
 	}
 
 	protected override void updateFurniture()
 	{
 		if (state == State.OPEN) {
-			furnaceLight.intensity = 0f;
-			animator.SetBool("open", false); // Not obvious... >_>
 			smoke.enableEmission = false;
+			if (source != null && takeSound != null) 
+			{
+				source.Stop ();
+				source.loop = false;
+				source.PlayOneShot (takeSound);
+				bellOnce = true;
+			}
 		} else {
-			furnaceLight.intensity = 1f;
-			animator.SetBool("open", true);
 			smoke.enableEmission = true;
+			if (source != null && processSound != null && putSound != null) 
+			{
+				source.loop = false;
+				source.PlayOneShot (putSound,0.25f);
+				source.loop = true;
+				source.Play();
+			}
 		}
 	}
 	protected override bool canProcess (AbstractFood food)
@@ -46,12 +62,18 @@ class AutoFurnace : abstractFurniture
 			loadBar.setLoading (value);
 			if (value == 0.05f) 
 			{
-				switch (content.getCookState ()) {
+				switch (content.getCookState ()) 
+				{
 				case Cooking.RAW:
 					loadBar.setColor (Color.green);
 					break;
 				case Cooking.COOKED:
 					loadBar.setColor (Color.yellow);
+					if (bellOnce) 
+					{
+						source.PlayOneShot (bellSound);
+						bellOnce = false;
+					}
 					break;
 				case Cooking.BURNED:
 					loadBar.setColor (Color.red);
